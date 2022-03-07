bc7a0dde700380f80adadb9cf4717efa182e0679
SIDDARTH.D <siddarth.dayasagar@gmail.com>
2022-03-07 20:43:28 +0530

    updated code

--- spot_simulation/scripts/kinematics.py
+++ spot_simulation/scripts/kinematics.py
@@ -15,10 +15,12 @@ class Kinematics:
         self.y=rospy.get_param('kinematics/Y')
         self.z=rospy.get_param('kinematics/Z')
         self.angle_pub=rospy.Publisher('/joint_state',gait_state,queue_size=10)
-
+        self.angles=[]
+        self.gait=gait_state()
+        self.rate=rospy.Rate(1)
     def inverseKinematics(self):
-        gait=gait_state()
-        rate=rospy.Rate(1)
+       
+       
         rospy.loginfo('PARAMETERS LOADED....')
         D1=(pow(self.x,2)+pow(self.y,2)-pow(self.link3,2)+pow(self.z,2)-pow(self.link2,2)-pow(self.link3,2))
         D2=2*self.link2*self.link3
@@ -26,19 +28,26 @@ class Kinematics:
         F=sqrt(pow(self.x,2)+pow(self.y,2)-pow(self.link1,2))
         theta1=-atan2(self.y,self.x)-atan2(F,-self.link1)
         theta3=atan2(sqrt(1-pow(D,2)),D)
-        theta2=atan2(self.z,F)-atan2(self.link3*sin(theta3),self.link2+self.link3*cos(theta3))
-
-        gait.status="RELAXING"
-        gait.theta1=degrees(theta1)
-        gait.theta2=degrees(theta2)
-        gait.theta3=degrees(theta3)
-
-        rospy.loginfo(gait)
-
-        while not rospy.is_shutdown():
-            self.angle_pub.publish(gait)
-            rate.sleep()
         
+        theta2=atan2(self.z,F)-atan2(self.link3*sin(theta3),self.link2+self.link3*cos(theta3))
+        
+
+
+        return theta1,theta2,theta3
+
+    def Sleep(self):
+
+        self.gait.status="Sleeping"
+        self.gait.angles=[0,0,0]
+        while not rospy.is_shutdown():
+            self.angle_pub.publish(self.gait)
+            self.rate.sleep()
+
+        rospy.loginfo(self.gait)
+
+        
+
+    
 
 
 
@@ -46,6 +55,6 @@ if __name__=="__main__":
 
     rospy.init_node('kinematics')
     spot=Kinematics()
-    spot.inverseKinematics()
+    spot.Sleep()
 
         
